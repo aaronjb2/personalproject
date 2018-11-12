a83f09c92a7e64bc71bc70b26e0fcc59665beb77
unknown <aaronjbraithwaite@gmail.com>
2018-11-12 12:11:18 -0700

    basic folder structure setup, basic server running

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+require('dotenv').config();
+const massive = require('massive');
+
+const app = express();
+
+app.use(express.json());
+
+const {SERVER_PORT, MASSIVE_CONNECTION} = process.env;
+
+massive(MASSIVE_CONNECTION).then(db=> {
+    app.search('db', db);
+    console.log('db is connected');
+})
+
+app.listen(SERVER_PORT, () => {
+    console.log(`On the ${SERVER_PORT}th day of Christmas my true love gave to me..... nothing because I'm single`);
+})
